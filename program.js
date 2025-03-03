2eb6158d91880bfd8c68f6c2de4f68365d5ab1ee
Paula andrade <paulaandz22@gmail.com>
2025-03-02 20:14:44 -0500

    v4

--- program.js
+++ program.js
@@ -5,45 +5,116 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-async function loadpolygon(){
+async function loadPolygon(){
     let myData = await fetch("AlamosNorte.geojson");
     let myPolygon = await myData.json();
-    L.geoJSON(myPolygon, 
+
+    L.geoJSON(myPolygon,
         {
             style:{
-                color:"blue"
+                color:'blue'
             }
-
         }
-
-
     ).addTo(map);
-
 }
 
-loadpolygon();
+loadPolygon();
 
-async function loadpolygon2(){
-    let myData = await fetch("arboles_AlamosNorte.geojson");
-    let myPolygon2 = await myData.json();
-    L.geoJSON(myPolygon2, 
+let btnTrees= document.getElementById("btnTrees");
+
+btnTrees.addEventListener('click',
+    async ()=>{
+        let response= await fetch("arboles_AlamosNorte.geojson");
+        let datos= await response.json();
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{                    
+                    return L.circleMarker(latlong,{
+                        radius:5,
+                        fillColor:'green',
+                        weight:1,
+                        opacity:1,
+                        fillOpacity: 0.5,
+                    })
+                }
+            }
+        ).addTo(map);
+    }
+)
+
+let btnAccidents= document.getElementById("btnAccidents");
+
+btnAccidents.addEventListener('click',
+    async ()=>{
+        let response= await fetch("Siniestros_AlamosNorte.geojson");
+        let datos= await response.json();
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{                    
+                    return L.circleMarker(latlong,{
+                        radius:5,
+                        fillColor:'red',
+                        weight:1,
+                        opacity:1,
+                        fillOpacity: 0.5,
+                    })
+                }
+            }
+        ).addTo(map);
+    }
+)
+
+let btnDistance= document.getElementById("btnDistance");
+
+btnDistance.addEventListener('click',
+    async ()=>{
+        let response= await fetch("arboles_AlamosNorte.geojson");
+        let datos= await response.json();
+        let trees= datos.features.map((myElement, index)=>({
+            id: index+1,
+            coordinates: myElement.geometry.coordinates
+        }));        
+
+        let distances=[];
+        trees.forEach( (treeA)=>{trees.forEach(
+
+            
+                (treeB)=>{
+                    if(treeA.id != treeB.id){
+                        let distance = turf.distance( 
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push(
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)                            
+                            ]
+                        )
+                }
+            }
+            )
+        }
+        )
+        generatePDF(distances, trees.lenght);
+    }
+)
+function generatePDF(distances, totalTrees){
+    let { jsPDF } = window.jspdf;
+    let documentPDF= new jsPDF();   
+    
+    documentPDF.text("REPORTE DE ÁRBOLES EN EL BARRIO GRAN BRITALIA", 10,10);
+
+    documentPDF.autoTable(
         {
-            style:{
-                color:"blue"
-            }
-
+            head: [['Árbol 1', 'Árbol 2', 'Distance']],
+            body: distances
         }
-
-
-    ).addTo(map);
-
+    );
+    documentPDF.save("Arboles.pdf")
 }
-
-loadpolygon2();
-
-
-let btntrees = document.getElementById('btntrees');
-btntrees.addEventListener("click",()=> alert("hola"));
-
-
-
